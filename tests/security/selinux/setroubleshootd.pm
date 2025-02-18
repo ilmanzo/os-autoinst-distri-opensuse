c87ac99d1c803b71b986a265654c0a2a8a9694db
Andrea Manzini <ilmanzo@gmail.com>
2025-02-18 14:54:34 +0100

    Add sealert tests

--- tests/security/selinux/setroubleshootd.pm
+++ tests/security/selinux/setroubleshootd.pm
@@ -8,8 +8,9 @@
 #    but after about 15 seconds it should be no longer active again.
 #  - Check setroubleshootd invoking via polkit as root, see
 #    /usr/share/dbus-1/system.d/org.fedoraproject.SetroubleshootFixit.conf
+#  - verify sealert working (sealert is part of setroubleshoot-server pkg)
 # Maintainer: QE Security <none@suse.de>
-# Tags: poo#174175
+# Tags: poo#174175, poo#174178
 
 use base "selinuxtest";
 use strict;
@@ -33,7 +34,7 @@ sub validate_service_restart {
     validate_script_output('systemctl restart setroubleshootd;systemctl is-active setroubleshootd.service;sleep 15;systemctl is-active setroubleshootd.service', sub { m/active.*inactive/s }, proceed_on_failure => 1);
 }
 
-sub validate_invocation_via_polkit() {
+sub validate_invocation_via_polkit {
     # check for invoking via polkit as root
     my $cmd = 'pkcheck -p $$ -a org.fedoraproject.setroubleshootfixit.write';
     assert_script_run qq{runuser root -c "$cmd"};
@@ -41,6 +42,54 @@ sub validate_invocation_via_polkit() {
     validate_script_output(qq{runuser bernhard -c "$cmd"},
         sub { m/GDBus.Error:org.freedesktop.PolicyKit1.Error.NotAuthorized: Only trusted callers/ },
         proceed_on_failure => 1);
+}
+
+sub valid_sealert_output {
+    my $output_to_validate = shift;
+    my @validations = (
+        qr /SELinux is preventing runcon from using the transition access on a process./,
+        qr /Selinux Enabled\s+True/,
+        qr /Policy Type\s+targeted/,
+        qr /Enforcing Mode\s+Enforcing/,
+        qr /Hash: runcon,unconfined_t,user_tmp_t,process,transition/,
+    );
+    foreach my $regex (@validations) {
+        return 0 if ($output_to_validate !~ $regex);
+    }
+    return 1;
+}
+
+sub check_sealert() {
+    # initially there should be no alerts (command should output nothing)
+    validate_script_output "sealert -l '*'", sub { m /^$/ };
+    # create a sample file and give it wrong http_content type on purpose
+    my $sample_file = '/tmp/sealert_sample_test';
+    assert_script_run "touch $sample_file && chcon -t httpd_sys_content_t $sample_file";
+    # ensure there are no alerts about that file as well
+    validate_script_output "sealert -l '*'", sub { m /^$/ };
+    # using user_tmp context, try to access the file (should fail)
+    die "Should deny permission" unless script_run("runcon -u guest_u -r guest_r -t user_tmp_t -- cat $sample_file");
+    # sealert should show deny message
+    my $sealert_output;
+    my $retries = 10;
+    while ($retries--) {
+        my $glob_output = script_output "sealert -l '*'";
+        record_info 'sealert output', $glob_output;
+        last if valid_sealert_output($glob_output);
+        sleep 1;
+    }
+    die "sealert -l '*' Does not validate" unless $retries;
+    # extract event id from the output
+    my $local_id;
+    if ($sealert_output =~ /Local ID\s+([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/s) {
+        $local_id = $1;
+    } else {
+        die "alert event ID not found";
+    }
+    # run same validations against specific ID output
+    die "sealert -l $local_id does not validate" unless valid_sealert_output(script_output "sealert -l $local_id");
+    # we should find deny message in journal as well
+    assert_script_run "journalctl -u setroubleshootd.service | grep 'SELinux is preventing runcon from using the transition access'";
 }
 
 sub run {
@@ -53,11 +102,12 @@ sub run {
     # ensure selinux is in enforcing mode
     validate_script_output 'getenforce', sub { m/Enforcing/ };
     # ensure pkg installation
-    zypper_call 'in setroubleshoot-server';
+    zypper_call 'in setroubleshoot-server setroubleshoot';
     assert_script_run 'rpm -q setroubleshoot-plugins';
     ensure_setroubleshootd_cannot_be_directly_run_as_root;
     validate_service_restart;
     validate_invocation_via_polkit;
+    check_sealert;
 }
 
 1;
