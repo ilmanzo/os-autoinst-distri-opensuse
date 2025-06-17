ca23a90adcef49a6e0d067253c72f85d250a80e2
Andrea Manzini <ilmanzo@gmail.com>
2025-06-17 16:31:42 +0200

    Schedule binary coverage for basic text utilities

---
+++ tests/coverage/coverage_setup.pm
@@ -0,0 +1,52 @@
+# Copyright SUSE LLC
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Summary: setup coverage tooling
+# Maintainer: Andrea Manzini <andrea.manzini@suse.com>
+
+# test data section in the YAML schedule must contain an hash
+# of packages to install -> binary to instrument. For example
+#
+# test_data:
+#   coverage_targets:
+#     unzip: /usr/bin/unzip
+#     nano: /usr/bin/nano
+
+use base 'opensusebasetest';
+use strict;
+use warnings;
+use testapi;
+use utils;
+use serial_terminal 'select_serial_terminal';
+use scheduler 'get_test_suite_data';
+
+sub run {
+    select_serial_terminal;
+    # enable debug repos
+    # on Tumbleweed does not have debuginfo in to-be-tested snapshots, except for selected package
+    # (see REPO_OSS_DEBUGINFO_PACKAGES)
+
+    my $test_data = get_test_suite_data();
+    # TODO maybe not the best way, but it works
+
+    while (my ($name, $url) = each(%{$test_data->{repositories}})) {
+        assert_script_run "zypper ar -e -f $url $name";
+    }
+
+    zypper_call '--gpg-auto-import-keys in ' . join ' ', @{$test_data->{extra_packages}};
+
+    assert_script_run 'export PIN_ROOT=/usr/lib64/coverage-tools/pin-root';
+
+    # install packages with debug info and wrap
+    # targets that will be instrumented for 'coverage'
+    while (my ($package, $binary_path) = each(%{$test_data->{coverage_targets}})) {
+        zypper_call "in $package $package-debuginfo";
+        assert_script_run "funkoverage wrap $binary_path";
+    }
+}
+
+sub test_flags {
+    return {fatal => 1, milestone => 1};
+}
+
+1;
