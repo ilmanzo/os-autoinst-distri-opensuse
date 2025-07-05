5e5f5d31f879877b5b1e6f563c2ada4da849f4e7
Andrea Manzini <ilmanzo@gmail.com>
2025-07-05 09:17:34 +0200

    demo for qesec unit testing

--- tests/fips/openjdk/openjdk_fips.pm
+++ tests/fips/openjdk/openjdk_fips.pm
@@ -17,16 +17,18 @@ use testapi;
 use utils;
 use openjdktest;
 use registration qw(add_suseconnect_product);
-use version_utils qw(is_sle is_sled is_rt is_sles4sap);
+use version_utils qw(is_sle is_sled is_rt);
+use security::openjdk_utils qw(get_java_versions);
 
-sub get_java_versions {
-    # on newer version we need legacy module for openjdk 11, but is not available
-    # on SLERT/SLED, can't test openjdk 11. On 15-SP7 17 is also in legacy module
-    return '21' if (is_rt || is_sled) && is_sle('>=15-SP7');
-    return '11 17 21' if (is_sle '>=15-SP6');
-    return '17 21' if ((is_rt || is_sled) && is_sle('>=15-SP6'));
-    return '11 17';
-}
+
+# sub get_java_versions {
+#     # on newer version we need legacy module for openjdk 11, but is not available
+#     # on SLERT/SLED, can't test openjdk 11. On 15-SP7 17 is also in legacy module
+#     return '21' if (is_rt || is_sled) && is_sle('>=15-SP7');
+#     return '11 17 21' if (is_sle '>=15-SP6');
+#     return '17 21' if ((is_rt || is_sled) && is_sle('>=15-SP6'));
+#     return '11 17';
+# }
 
 sub run {
     my $self = @_;
