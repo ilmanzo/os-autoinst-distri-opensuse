09bcf8063a887ffa5462e6553bced81db7bc15e0
Andrea Manzini <ilmanzo@gmail.com>
2026-04-17 09:38:01 +0200

    Adapt the schedule to the new eBPF tracer

--- tests/coverage/coverage_setup.pm
+++ tests/coverage/coverage_setup.pm
@@ -4,13 +4,19 @@
 # Summary: setup coverage tooling
 # Maintainer: Andrea Manzini <andrea.manzini@suse.com>
 
-# test data section in the YAML schedule must contain an hash
-# of packages to install -> binary to instrument. For example
+# test_data in the YAML schedule must contain:
+#   coverage_targets: hash of package_name -> binary (scalar) or [list of binaries]
+#   helper_packages:  (optional) packages needed to make tests work, but NOT instrumented
 #
-# test_data:
-#   coverage_targets:
-#     unzip: /usr/bin/unzip
-#     nano: /usr/bin/nano
+# Example:
+#   test_data:
+#     helper_packages:
+#       - wget
+#     coverage_targets:
+#       curl: /usr/bin/curl
+#       net-snmp:
+#         - /usr/bin/snmpget
+#         - /usr/bin/snmpset
 
 use Mojo::Base 'opensusebasetest';
 use testapi;
@@ -56,27 +62,33 @@ sub run {
         assert_script_run "zypper ar -e -f $url $name";
     }
 
-    # install coverage tools and any extra packages + debug info
-    my @packages;
-    for (@{$test_data->{extra_packages}}) {
-        push @packages, $_;
-        push @packages, $_ . '-debuginfo';
+    # install coverage tools + debuginfo, and any helper packages
+    my (@packages, @binaries);
+    while (my ($pkg, $targets) = each(%{$test_data->{coverage_targets}})) {
+        push @packages, $pkg, $pkg . '-debuginfo';
+        my @pkg_bins = ref($targets) eq 'ARRAY' ? @$targets : ($targets);
+        push @binaries, @pkg_bins;
     }
-    push @packages, 'coverage-tools';
+    for (@{$test_data->{helper_packages} // []}) {
+        push @packages, $_;
+    }
+    push @packages, 'coverage-tools', 'elfutils';
     zypper_call '--gpg-auto-import-keys in ' . join ' ', @packages;
 
     # sets up the environment for coverage
-    assert_script_run 'export LOG_DIR=/var/coverage/data';
-    assert_script_run 'export PIN_ROOT=/usr/lib64/coverage-tools/pin';
+    my $log_dir = '/var/coverage/data';
 
-    # add the environment setup to .bashrc for future sessions (root and plain user)
-    assert_script_run 'echo "LOG_DIR=/var/coverage/data" > /etc/bash.bashrc.local';
-    assert_script_run 'echo "PIN_ROOT=/usr/lib64/coverage-tools/pin" >> /etc/bash.bashrc.local';
+    assert_script_run "mkdir -m 0777 -p $log_dir";
 
-    assert_script_run "mkdir -m 0777 -p \$LOG_DIR";
+    # all shared libraries should have execution bit set, otherwise the instrumentation will fail
+    # both in /lib and /usr/lib, and in their 64-bit counterparts
+    assert_script_run 'find /lib* /usr/lib* -type f -executable -exec chmod +x {} +';
+
+    # set capabilities to run eBPF programs without root privileges
+    assert_script_run 'funkoverage setup';
 
     # wrap the binaries that will be instrumented for 'coverage'
-    assert_script_run "funkoverage wrap " . join ' ', @{$test_data->{coverage_targets}};
+    assert_script_run "funkoverage install " . join ' ', @binaries;
 }
 
 sub test_flags {
